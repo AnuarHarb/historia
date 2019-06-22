6c92ac98a6330d1889e309440d92b8e3e52a5491
Anuar Harb <anuarharb@hotmail.com>
2019-06-22 11:22:02 -0500

    version 2

--- scripts.js
+++ scripts.js
@@ -1,28 +1,18 @@
 
 
-
-
-//Elementos del documento
 const resultText = document.querySelector('.result-text');
 const editButton = document.querySelector('.edit-button');
-const protagonistaInput = document.querySelector('.protagonista-input');
+const editProtagonista = document.querySelector('.edit-protagonista');
+const editLugar = document.querySelector('.edit-lugar');
 
-// Suscripciones a Eventos
 editButton.addEventListener('click', function() {
-  let inputValue = protagonistaInput.value;
-  escribirHistoria(inputValue);
+  const protagonistaValue = editProtagonista.value;
+  const lugarValue = editLugar.value;
+
+  escribirHistoria(protagonistaValue, lugarValue);
 });
 
-// Función que cambia la historia
-function escribirHistoria(protagonista) {
-  // let protagonista = 'Anuar';
-  resultText.innerText = 'Hola ' + protagonista + ', esta es la historia que estoy agregando desde js';
+function escribirHistoria(protagonista, lugar) {
+  resultText.innerText = 'Hola ' + protagonista + ', esta es una historia que estoy agregando a ' + lugar + ' con js';
+  resultText.style.fontSize = '40px';
 }
-
-
-
-
-
-
-
-// console.log(resultText.innerText);
